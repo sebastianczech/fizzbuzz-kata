11a3de71e7c90f4bf73ebbf5ba95fde2224b42f7
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2017-10-19 00:35:58 +0200

    Update dependencies, add gradle build and a starting test.

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,14 @@
 package tdd.fizzbuzz;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzTest {
 
+    @Test
+    public void startHere() {
+        assertThat(4).isEqualTo(5);
+    }
 }
