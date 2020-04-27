4518de289f0520e1fdb31a384f6a73de93b1572f
Mateusz Nowak <mateusz.nowak@vonage.com>
2020-04-27 11:03:09 +0200

    Update JUnit to 5 and Gradle to 6

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,7 +1,6 @@
 package tdd.fizzbuzz;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
