3685486419efb5a3ac56537aeb549628a9f008b7
michal.lipski <kiwaczki@gmail.com>
2020-05-31 00:46:14 +0200

    solution for 1 requirement

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,31 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+
+    public String fizzBuzz(int number) {
+        if(isDividableByThree(number) && isDividableByFive(number)){
+            return FIZZ + BUZZ;
+        }
+        if (isDividableByThree(number)) {
+            return FIZZ;
+        }
+        if (isDividableByFive(number)) {
+            return BUZZ;
+        }
+        return String.valueOf(number);
+    }
+
+    private boolean isDividableByFive(int number) {
+        return isDividableBy(number, 5);
+    }
+
+    private boolean isDividableByThree(int number) {
+        return isDividableBy(number, 3);
+    }
+
+    private boolean isDividableBy(int number, int dividedBy) {
+        return number % dividedBy == 0;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,46 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
-    @Test
-    public void startHere() {
-        assertThat(4).isEqualTo(5);
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
     }
+
+    @Test
+    public void should_return_number_when_not_dividable_by_3_and_5() {
+        String fizzBuzzResult = fizzBuzz.fizzBuzz(1);
+
+        assertThat(fizzBuzzResult).isEqualTo("1");
+    }
+
+    @Test
+    void should_return_Fizz_when_dividable_by_3() {
+        String result = fizzBuzz.fizzBuzz(3);
+
+        assertThat(result).isEqualTo("Fizz");
+    }
+
+    @Test
+    void should_return_Buzz_when_dividable_by_5() {
+        String result = fizzBuzz.fizzBuzz(5);
+
+        assertThat(result).isEqualTo("Buzz");
+    }
+
+    @Test
+    void should_return_FizzBuzz_when_dividable_by_3_and_5() {
+        String result = fizzBuzz.fizzBuzz(15);
+
+        assertThat(result).isEqualTo("FizzBuzz");
+    }
+
+
 }
