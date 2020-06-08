626ea625313c8f7fee21985c62744f4e4588dfa2
Sebastian Czech <sebaczech@gmail.com>
2020-06-08 23:56:20 +0200

    refactor: Zmiany w kodzie klasy FizzBuzz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,17 +2,26 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
-    public String fizzBuzz(int i) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            return "FizzBuzz";
-        } else if (i % 5 == 0 || String.valueOf(i).contains("5")) {
-            return "Fizz";
-        } else if (i % 7 == 0 || String.valueOf(i).contains("7")) {
-            return "Buzz";
-        } else if (i % 9 == 0 || String.valueOf(i).contains("9")) {
-            return "Bar";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String BAR = "Bar";
+
+    public String fizzBuzz(int number) {
+        if (isDIvidableByNumOrContainsNum(number, 3)
+                && isDIvidableByNumOrContainsNum(number, 5)) {
+            return FIZZ + BUZZ;
+        } else if (isDIvidableByNumOrContainsNum(number, 5)) {
+            return FIZZ;
+        } else if (isDIvidableByNumOrContainsNum(number, 7)) {
+            return BUZZ;
+        } else if (isDIvidableByNumOrContainsNum(number, 9)) {
+            return BAR;
         } else {
-            return String.valueOf(i);
+            return String.valueOf(number);
         }
+    }
+
+    private boolean isDIvidableByNumOrContainsNum(int number, int divider) {
+        return number % divider == 0 || String.valueOf(number).contains(String.valueOf(divider));
     }
 }
